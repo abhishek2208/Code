735d321da11b2c183c22655bf565c6ca22fc8bf5
abhishek2208 <abhishekhitaishi2208@gmail.com>
2017-05-31 00:03:34 +0530

    Add files via upload

---
+++ Delete  Linked List.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    struct node* next;
+};
+struct node* append(struct node* head,int val)
+{
+    struct node* new_node=(struct node*)malloc(sizeof(struct node));
+    new_node->data=val;
+    struct node* temp=head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=new_node;
+    new_node->next=NULL;
+    return head;
+
+}
+void printList(struct node *node)
+{
+  while (node != NULL)
+  {
+     cout<<node->data<<" ";
+     node = node->next;
+  }
+  cout<<endl;
+}
+struct node* delete_list(struct node* head)
+{
+  if (head==NULL)
+    return head;
+
+  struct node* curr=head;
+  struct node* next;
+  while(curr!=NULL)
+  {
+      next=curr->next;
+      free(curr);
+      curr=next;
+  }
+  head=NULL;
+  return head;
+
+
+
+};
+
+int main()
+{
+    struct node* head= (struct node*)malloc(sizeof(struct node));
+    head->data=4;
+    head->next=NULL;
+    head=append(head,5);
+    head=append(head,6);
+    head=append(head,7);
+    head=append(head,8);
+    head=append(head,9);
+    printList(head);
+    head=delete_list(head);
+    printList(head);
+
+
+
+
+
+}
+
