8e75898807e3033470dac625c2826f7f0f45696e
abhishek2208 <abhishekhitaishi2208@gmail.com>
2017-06-04 18:36:37 +0530

    Add files via upload

---
+++ Delete Tree.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+struct node* newnode(int d)
+{
+    struct node* root=(struct node*)malloc(sizeof(struct node));
+    root->data=d;
+    root->left=NULL;
+    root->right=NULL;
+};
+
+void delete_tree(struct node* root)
+{
+ if(root==NULL)
+    return ;
+
+    delete_tree(root->left);
+    delete_tree(root->right);
+    free(root);
+
+}
+
+int main()
+{
+   struct  node *root = newnode(1);
+    root->left = newnode(2);
+    root->right = newnode(3);
+    root->left->left = newnode(4);
+    root->left->right = newnode(5);
+    root->right->right = newnode(6);
+
+
+
+    delete_tree(root);
+    cout<<root->data;
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
